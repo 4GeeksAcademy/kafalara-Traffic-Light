94e85644705dbcd70c8b021da9f77c4e4032a14d
Kafalara <karen.larrosa@gmail.com>
2024-10-09 21:29:18 +0000

    Proyecto entregado Traffic Light, con ternarios y template literals

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+const TrafficLight = () => {
+  // El componente debe tener un estado que almacene el color actual que debe brillar, para ello uso el hook useState de la siguiente manera:
+  const [color, setColor] = useState("red");
+
+  return (
+    <div id="container">
+      <div
+        className={`light red ${color === "red" ? "selected" : ""}`}
+        onClick={() => setColor("red")}
+      ></div>
+      <div
+        className={`light yellow ${color === "yellow" ? "selected" : ""}`}
+        onClick={() => setColor("yellow")}
+      ></div>
+      <div
+        className={`light green ${color === "green" ? "selected" : ""}`}
+        onClick={() => setColor("green")}
+      ></div>
+    </div>
+  );
 };
 
-export default Home;
+// Utiliza ReactDOM.render para procesar el componente en el DOM
+ReactDOM.render(<TrafficLight />, document.querySelector("#app"));
+
+export default TrafficLight;
